f6711b519196ec0a6b2c144126f94138c4a205ee
Vitaly Bogryakov <VitJRBOG@users.noreply.github.com>
2021-04-20 10:52:10 +0500

    Solved 7th exercise

--- e7/solution_test.go
+++ e7/solution_test.go
@@ -1,1 +1,19 @@
 package e7
+
+import (
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	input := []int{120, 1534236469, -2147483648, -2147483412}
+
+	expected := []int{21, 0, 0, -2143847412}
+
+	for i := 0; i < len(input); i++ {
+		result := Reverse(input[i])
+
+		if result != expected[i] {
+			t.Fatalf("\nbad result for %d:\ngot %d\nexpectations %d\n", input[i], result, expected[i])
+		}
+	}
+}
