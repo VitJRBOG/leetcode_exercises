319636a51e8b2d5b46d827776d6a9de0bdadcb91
Vitaly Bogryakov <VitJRBOG@users.noreply.github.com>
2021-04-22 11:09:20 +0500

    Solved 2nd exercise

--- e2/solution.go
+++ e2/solution.go
@@ -6,89 +6,38 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	num1 := nodeToListInt(l1)
-	num2 := nodeToListInt(l2)
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	lHead := ListNode{0, nil}
+	l3 := &lHead
 
-	var nums []int
-	if len(num1) == len(num2) {
-		nums = calcNums(num1, num2)
-	} else {
-		if len(num1) > len(num2) {
-			nums = calcNums(num2, num1)
-		} else {
-			nums = calcNums(num1, num2)
-		}
-	}
+	lx := l1
+	ly := l2
+	carry := 0
 
-	result := intListToNode(nums)
-
-	return result
-}
-
-func nodeToListInt(n *ListNode) []int {
-	var nums []int
 	for {
-		if n != nil {
-			nums = append(nums, n.Val)
-			n = n.Next
-		} else {
+		if lx == nil && ly == nil && carry == 0 {
 			break
 		}
-	}
 
-	return nums
-}
+		lxVal := 0
+		lyVal := 0
 
-func calcNums(num1, num2 []int) []int {
-	var nums []int
-
-	num1 = reverseIntList(num1)
-	num2 = reverseIntList(num2)
-
-	for i := 0; i < len(num2); i++ {
-		if i < len(num1) {
-			nums = append(nums, num2[i]+num1[i])
-		} else {
-			nums = append(nums, num2[i])
+		if lx != nil {
+			lxVal = lx.Val
+			lx = lx.Next
 		}
-	}
 
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > 9 {
-			nums[i] -= 10
-			if i == len(nums)-1 {
-				nums = append(nums, 1)
-			} else {
-				nums[i+1] += 1
-			}
+		if ly != nil {
+			lyVal = ly.Val
+			ly = ly.Next
 		}
+
+		sum := lxVal + lyVal + carry
+		carry = sum / 10
+
+		l3.Next = &ListNode{sum % 10, nil}
+		l3 = l3.Next
 	}
 
-	nums = reverseIntList(nums)
-
-	return nums
-}
-
-func reverseIntList(origList []int) []int {
-	var newList []int
-
-	for i := len(origList) - 1; i >= 0; i-- {
-		newList = append(newList, origList[i])
-	}
-
-	return newList
-}
-
-func intListToNode(nums []int) *ListNode {
-	var n *ListNode
-	for i := 0; i < len(nums); i++ {
-		if i == 0 {
-			n = &ListNode{nums[i], nil}
-		} else {
-			rn := ListNode{nums[i], n}
-			n = &rn
-		}
-	}
-	return n
+	return lHead.Next
 }
--- e2/solution_test.go
+++ e2/solution_test.go
@@ -1,69 +1,56 @@
 package e2
 
 import (
-	"strconv"
-	"strings"
 	"testing"
 )
 
 func TestAddTwoNumbers(t *testing.T) {
+	input1 := [][]int{{9, 9, 9, 9, 9, 9, 9}, {2, 4, 3}, {0}}
+	input2 := [][]int{{9, 9, 9, 9}, {5, 6, 4}, {0}}
 
-	num1 := []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
-	num2 := []int{5, 6, 4}
+	expectations := [][]int{{8, 9, 9, 9, 0, 0, 0, 1}, {7, 0, 8}, {0}}
 
-	var l1 *ListNode
-	for i := 0; i < len(num1); i++ {
-		if i == 0 {
-			l1 = &ListNode{num1[i], nil}
-		} else {
-			rn := ListNode{num1[i], l1}
-			l1 = &rn
+	for i := 0; i < len(input1); i++ {
+
+		l1Head := ListNode{0, nil}
+		l1 := &l1Head
+
+		for n := 0; n < len(input1[i]); n++ {
+			l1.Next = &ListNode{input1[i][n], nil}
+			l1 = l1.Next
 		}
-	}
 
-	var l2 *ListNode
-	for i := 0; i < len(num2); i++ {
-		if i == 0 {
-			l2 = &ListNode{num2[i], nil}
-		} else {
-			rn := ListNode{num2[i], l2}
-			l2 = &rn
+		l2Head := ListNode{0, nil}
+		l2 := &l2Head
+
+		for n := 0; n < len(input2[i]); n++ {
+			l2.Next = &ListNode{input2[i][n], nil}
+			l2 = l2.Next
 		}
-	}
 
-	expected := []int{6, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+		l3 := addTwoNumbers(l1Head.Next, l2Head.Next)
+		var lz *ListNode
+		lz = l3
 
-	result := AddTwoNumbers(l1, l2)
-
-	var strResult string
-	for {
-		if result != nil {
-			strResult += strconv.Itoa(result.Val)
-			result = result.Next
-		} else {
-			break
-		}
-	}
-	s := strings.Split(strResult, "")
-
-	var r []int
-
-	for _, item := range s {
-		n, err := strconv.Atoi(item)
-		if err != nil {
-			panic(err.Error())
-		}
-		r = append(r, n)
-	}
-
-	if len(r) == len(expected) {
-		for i := 0; i < len(r); i++ {
-			if r[i] != expected[i] {
-				t.Fatalf("bad result for %v and %v\ngot %v\nexpected %v", num1, num2, r, expected)
+		var result []int
+		for {
+			result = append(result, lz.Val)
+			if lz.Next != nil {
+				lz = lz.Next
+			} else {
+				break
 			}
 		}
-	} else {
-		t.Fatalf("bad result for %v and %v\ngot %v\nexpected %v", num1, num2, r, expected)
-	}
 
+		if len(result) != len(expectations[i]) {
+			t.Fatalf("\nbad result for %d and %d\nexpected %d\ngot %d\n", input1[i], input2[i], expectations[i], result)
+		} else {
+			for n := 0; n < len(result); n++ {
+				if result[n] != expectations[i][n] {
+					t.Fatalf("\nbad result for %d and %d\nexpected %d\ngot %d\n", input1[i], input2[i], expectations[i], result)
+					break
+				}
+			}
+		}
+	}
 }
