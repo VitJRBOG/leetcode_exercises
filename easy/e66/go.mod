adcd0e4c5026f1edd8a1781045a7f3c2127e8d32
Vitaly Bogryakov <VitJRBOG@users.noreply.github.com>
2021-05-27 11:03:20 +0500

    Solved 66th problem

---
+++ easy/e66/go.mod
@@ -0,0 +1,3 @@
+module github.com/VitJRBOG/leetcode_exercises/easy/e66
+
+go 1.16
---
+++ easy/e66/solution_test.go
@@ -0,0 +1,29 @@
+package e66
+
+import "testing"
+
+func TestPlusOne(t *testing.T) {
+	input := [][]int{{1, 2, 3}, {4, 3, 2, 1}, {0}, {9}}
+	expectations := [][]int{{1, 2, 4}, {4, 3, 2, 2}, {1}, {1, 0}}
+
+	if len(input) == len(expectations) {
+		for i := 0; i < len(input); i++ {
+
+			result := plusOne(input[i])
+
+			if len(result) != len(expectations[i]) {
+				t.Fatalf("\nbad result for %d\nexpected: %d\ngot: %d\n",
+					input[i], expectations[i], result)
+			} else {
+				for n := 0; n < len(result); n++ {
+					if result[n] != expectations[i][n] {
+						t.Fatalf("\nbad result for %d\nexpected: %d\ngot: %d\n",
+							input[i], expectations[i], result)
+					}
+				}
+			}
+		}
+	} else {
+		t.Fatalf("\nwrong test: different length of input and expectations slices\n")
+	}
+}
